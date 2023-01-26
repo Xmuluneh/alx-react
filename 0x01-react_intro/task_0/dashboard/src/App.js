875b84c48d1e31d2b22b96032bc9b066cc30f0c0
Xmuluneh <mulabr673@gmail.com>
2023-01-26 13:11:03 +0300

    Task Zero

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,21 +1,21 @@
-import logo from './HolbertonLogo.ico';
+import logo from './HolbertonLogo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo "alt= "logo"/>
         <h1>School dashboard</h1>
       </div>
-      <div>
-        <div className="App-body"></div>
+      <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
       <div className="App-footer">
         <p>Copyright 2020 - holberton School</p>
       </div>
-    </div>
+      </div>
+    
   );
 }
 
