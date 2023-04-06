f46eca07cde6145b6f65b4208e984a63ca540d28
Xmuluneh <mulabr673@gmail.com>
2023-04-06 16:09:45 +0300

    Task completed

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -1,28 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import {getFooterCopy,getFullYear} from './utils'
+import logo from './HolbertonLogo.jpg';
+import "./App.css";
+import { getFooterCopy, getFullYear } from './utils';
+
 function App() {
   return (
     <div className="App">
-    <div className="App-header">
-    <img src={logo} className="App-logo "alt= "logo"/>
-      <h1>School dashboard</h1>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>School dashboard</h1>
+        </div>
+        <div className="App-body">
+          <p>Login to access the full dashboard</p>
+          <label htmlFor='email'>Email: </label>
+          <input type="email" id='email' />
+          <label htmlFor='pass'>Password: </label>
+          <input type="password" id='pass'/>
+          <button>OK</button>
+        </div>
+        <div className="App-footer">
+          <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+        </div>
     </div>
-    <div className="App-body">
-      <p>Login to access the full dashboard</p>
-      <form>
-        <label htmlFor='email'>Email</label>
-        <input id='email' type='email'></input>
-        <label htmlFor='password'>password</label>
-        <input id='password' type='password'></input>
-         <button>OK</button>
-      </form>
-    </div>
-    <div className="App-footer">
-      <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
-    </div>
-    </div>
-  
   );
 }
 
